db43786734a10641790dbaf44ef8a37e797cb3dd
Offiong Aniekan <gentleekan@gmail.com>
2017-04-28 16:57:56 -0700

    Correction to Database Credentials change ; to ,

--- config/database.php
+++ config/database.php
@@ -48,11 +48,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host; //env('DB_HOST', '127.0.0.1'),
+            'host' => $host, //env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database; //env('DB_DATABASE', 'forge'),
-            'username' => $username; //env('DB_USERNAME', 'forge'),
-            'password' => $password; //env('DB_PASSWORD', ''),
+            'database' => $database, //env('DB_DATABASE', 'forge'),
+            'username' => $username, //env('DB_USERNAME', 'forge'),
+            'password' => $password, //env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
