5a138c8a23241c368e27c9839b47f6be539d60ec
Offiong Aniekan <gentleekan@gmail.com>
2017-07-23 13:37:39 +0100

    Removing User seed due to Faker Require-Dev Dependency

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeed::class);
         $this->call(BankSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(RecurrenceSeeder::class);
