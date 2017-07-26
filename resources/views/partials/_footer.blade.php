0099fdf1f4ffab72765f69a9ebd547d43e845d3d
Offiong Aniekan <gentleekan@gmail.com>
2017-07-26 05:23:09 +0100

    Adding _footer.blade.php for auth page

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer navbar-fixed-bottom">
+    <h5 class="padding-top-lg padding-bottom-lg text-center bg-primary text-white">
+        <strong>Copyright &copy; {{ date('Y') . ' ' . config('app.name') }} All rights reserved.</strong>
+    </h5>
+</footer>        
