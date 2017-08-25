09486e81b172cf335b5b96a38ccf12b8cf3cc4fe
Offiong Aniekan <gentleekan@gmail.com>
2017-08-25 10:49:45 +0100

    Changes for tos, privacy pages

--- resources/views/legal/privacy.blade.php
+++ resources/views/legal/privacy.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.auth')
 
+@section('added_css')
+    <link href="{{ asset('css/spa.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
     <div class="content panel">
         <h3 class="text-center text-info">PRIVACY POLICY</h3><hr />
